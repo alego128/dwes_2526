c9c207b0d205499f2347bb649d178e82d21cc1f6
Alejandro González <152494270+alego128@users.noreply.github.com>
2025-10-01 11:05:50 +0200

    Add files via upload

---
+++ Tema 2/Ejercicios/Actividades/Ejercicio1/Ejercicio2.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1>Bienvenido a mi página en PHP</h1>";
+
+echo "<p>
+Este es un ejemplo de página generada con PHP. <br>
+</p>";
+
+echo "<a href='http://www.elpais.es'>Visitar El País</a>";
+echo "<img src='we.avif' alt='Logo El País' width='200'>";
+
+
+?>
---
+++ Tema 2/Ejercicios/Ejemplos/Ejemplo5.php
@@ -0,0 +1,16 @@
+<?php
+
+    /* 
+    Ejemplo 2.5
+    Alejandro González Román
+    Definición y uso de constantes
+    */
+
+    define("PI", 3.1416);
+    define("TASA_IVA", 0.21);
+    define("NOMBRE_EMPRESA", "ACME S.A.");
+
+    echo "<p>Valor de PI: " . PI . "</p>";
+    echo "<p>Tasa de IVA: " . TASA_IVA . "</p>";
+    echo "<p>Nombre de la empresa: " . NOMBRE_EMPRESA . "</p>";
+?>
